262202360e7f77f7515641e937eed6b5f358bbe2
Tsukasa-Igarashi <tks.50arashi@outlook.jp>
2020-03-02 23:40:36 +0900

    ボタンを押すと自分の手の画像が出る

--- js/janken.js
+++ js/janken.js
@@ -1,23 +1,25 @@
-//var me = [gu, ch, pa];
-// var ch = "images/ch.png";
-// var pa = "images/pa.png";
 var jan = ["images/gu.png", "images/ch.png", "images/pa.png"];
 var random;
 var me;
 var ran = setInterval(com_jan, 100);
 
+
 function com_jan() {
-
     random = Math.floor(Math.random() * jan.length);
-
     document.querySelector(".hand").innerHTML = "<img src=" + jan[random] + ">";
+    // document.querySelector(".me_img").innerHTML = "<img src=" + jan[random] + ">";
 }
 
-function me_jan() {
-    var me = document.getElementsByClassName('btn');
-    me.addEventLinearAll('onclick', function() {
-        if(me.innerHTML === "グー") {
+var btn = document.querySelectorAll(".me_jan");
+
+for (var i = 0; btn.length > i; i++) {
+    btn[i].addEventListener('click', function () {
+        if(this.innerHTML == "グー"){
             document.querySelector(".me_img").innerHTML = "<img src=" + jan[0] + ">";
+        }else if(this.innerHTML == "チョキ") {
+            document.querySelector(".me_img").innerHTML = "<img src=" + jan[1] + ">";
+        }else {
+            document.querySelector(".me_img").innerHTML = "<img src=" + jan[2] + ">";
         }
     });
 }
