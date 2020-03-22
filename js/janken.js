db5054e4520d58172cd22425c1f31a760be8efde
Tsukasa-Igarashi <tks.50arashi@outlook.jp>
2020-03-22 23:25:28 +0900

    second commit

--- js/janken.js
+++ js/janken.js
@@ -8,18 +8,43 @@ function com_jan() {
     random = Math.floor(Math.random() * jan.length);
     document.querySelector(".hand").innerHTML = "<img src=" + jan[random] + ">";
     // document.querySelector(".me_img").innerHTML = "<img src=" + jan[random] + ">";
+    if (document.querySelector(".me_img").innerHTML !== "") {
+        clearInterval(ran);
+        if(document.querySelector(".me_img").innerHTML === document.querySelector(".hand").innerHTML) {
+            document.querySelector("#vs").innerHTML = "引き分け";
+        } else if ((document.querySelector(".me_img").innerHTML == "<img src=" + jan[0] + ">" && document.querySelector(".hand").innerHTML == "<img src=" + jan[1] + ">") || (document.querySelector(".me_img").innerHTML == "<img src=" + jan[1] + ">" && document.querySelector(".hand").innerHTML == "<img src=" + jan[2] + ">") || (document.querySelector(".me_img").innerHTML == "<img src=" + jan[2] + ">" && document.querySelector(".hand").innerHTML == "<img src=" + jan[0] + ">")) {
+            document.querySelector("#vs").innerHTML = "勝ち";
+        } else {
+            document.querySelector("#vs").innerHTML = "負け";
+        }
+        // if (((document.querySelector(".me_img").innerHTML == "<img src=" + jan[0] + ">") === (document.querySelector(".hand").innerHTML == "<img src=" + jan[0] + ">")) || ((document.querySelector(".me_img").innerHTML == "<img src=" + jan[1] + ">") === (document.querySelector(".hand").innerHTML == "<img src=" + jan[1] + ">")) || ((document.querySelector(".me_img").innerHTML == "<img src=" + jan[2] + ">") === (document.querySelector(".hand").innerHTML == "<img src=" + jan[2] + ">"))) {
+        //     document.querySelector("#vs").innerHTML = "引き分け";
+        // } else if (((document.querySelector(".me_img").innerHTML == "<img src=" + jan[0] + ">") && (document.querySelector(".hand").innerHTML == "<img src=" + jan[random] + ">")) || ((document.querySelector(".me_img").innerHTML == "<img src=" + jan[1] + ">") && (document.querySelector(".hand").innerHTML == "<img src=" + jan[random] + ">")) || ((document.querySelector(".me_img").innerHTML == "<img src=" + jan[2] + ">") && (document.querySelector(".hand").innerHTML == "<img src=" + jan[random] + ">"))) {
+        //     document.querySelector("#vs").innerHTML = "勝ち";
+        // } else {
+        //     document.querySelector("#vs").innerHTML = "負け";
+        // }
+    }
 }
 
 var btn = document.querySelectorAll(".me_jan");
 
 for (var i = 0; btn.length > i; i++) {
     btn[i].addEventListener('click', function () {
-        if(this.innerHTML == "グー"){
+        if (this.innerHTML == "グー") {
             document.querySelector(".me_img").innerHTML = "<img src=" + jan[0] + ">";
-        }else if(this.innerHTML == "チョキ") {
+        } else if (this.innerHTML == "チョキ") {
             document.querySelector(".me_img").innerHTML = "<img src=" + jan[1] + ">";
-        }else {
+        } else {
             document.querySelector(".me_img").innerHTML = "<img src=" + jan[2] + ">";
         }
     });
 }
+
+var btn_more = document.querySelector("#more_jan");
+btn_more.addEventListener('click', function () {
+    document.querySelector(".me_img").innerHTML = "";
+    document.querySelector("#vs").innerHTML = "vs";
+    ran = setInterval(com_jan,100);
+});
+
